6d347f58a720562b8eb24ddd9a3e2c99d949cdce
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-12 00:27:27 +0530

    Commiting to uc3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,15 +8,22 @@ namespace ConsoleApp1
         {
             int WagePerHr = 20;
             Random random = new Random();
-            int value = random.Next(0, 2);
+            int value = random.Next(0, 3);
             if (value == 0)
             {
                 Console.WriteLine("Employee is absent");
             }
+            else if (value == 1)
+            {
+                Console.WriteLine("Employee is working part time");
+                int Salary = WagePerHr * 4;
+                Console.WriteLine("Salary for part time is " + Salary);
+            }
             else
             {
+                Console.WriteLine("Employee is working full time");
                 int Salary = WagePerHr * 8;
-                Console.WriteLine(Salary);
+                Console.WriteLine("Salary for full time is " + Salary);
             }
         }
     }
