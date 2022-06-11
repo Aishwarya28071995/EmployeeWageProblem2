ae6451543a27501b004990f4c0a2027a9459e18f
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-12 00:22:34 +0530

    Commiting files to UC1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            if (value == 0)
+            {
+                Console.WriteLine("Employee is absent");
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+                Console.WriteLine(value);
+            }
         }
     }
 }
